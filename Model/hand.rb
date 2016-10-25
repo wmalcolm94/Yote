212010477f5b08c07e0fa8afa213486f6ca41104
wmalcolm94 <wmalcolm94@outlook.com>
2016-10-25 16:54:21 -0400

    added hand skeleton

---
+++ Model/hand.rb
@@ -0,0 +1,16 @@
+pieces = []
+
+def initialize(Player owner)
+    for i in 0..12
+        pieces[i] = Piece.new
+    end
+end
+
+def removePiece 
+    #which piece do you remove???
+    
+end
+
+def getCount
+    return pieces.length
+end
