e54efec19df76f285e3fe0179621bfb21ba8784f
wmalcolm94 <wmalcolm94@outlook.com>
2016-10-25 16:57:41 -0400

    added Hand class

--- Model/hand.rb
+++ Model/hand.rb
@@ -1,16 +1,17 @@
-pieces = []
+class Hand
+    pieces = []
 
-def initialize(Player owner)
-    for i in 0..12
-        pieces[i] = Piece.new
+    def initialize(Player owner)
+        for i in 0..12
+            pieces[i] = Piece.new
+        end
     end
-end
 
-def removePiece 
-    #which piece do you remove???
-    
-end
+    def removePiece 
+        #which piece do you remove???
+    end
 
-def getCount
-    return pieces.length
+    def getCount
+        return pieces.length
+    end
 end
