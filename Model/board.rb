244ab7d37f00aadbf9b1335fb7c5782f7d3c4818
Ethan <evanhout@uoguelph.ca>
2016-10-25 20:14:07 -0400

    No im not done yeet you fuckwits. im just making sure we have proper version control. You are fucking welcome. Contains working(PROBABLY) canMove, jump, move as well as some other bs i needed for canMove Your Friend and Team Liason, Ethan

--- Model/board.rb
+++ Model/board.rb
@@ -1,46 +1,71 @@
+require Cell
+require Player
+require Piece
+
 class Board
   
-    Cell[5][6] cells
-
-    def jump(int[2] src_cell, int[2] dest_cell)
+    @cells = Cells[5][6]
+        
+    #for all the following src_cell and dest_cell should be a integer array of size 2
+    def jump(src_cell, dest_cell)
+        
         temp = cells[src_cell[0]][src_cell[1]].removePiece()
         cells[dest_cell[0]][dest_cell[1]].removePiece()
         cells[dest_cell[0]][dest_cell[1]].addPiece(temp)
-    end
-
-    def move(int[2] src_cell, int[2] dest_cell)
     
     end
 
-    def canJump(int[2] src_cell)
+    def move(src_cell,dest_cell)
+        
+        temp = cells[src_cell[0][src_cell[1]]].removePiece()
+        cells[dest_cell[0]][dest_cell[1]].addPiece(temp)
+
+    end
+
+    def canJump( src_cell)
+    end
+
+    def canMove(src_cell)
+        adjacent = getAdjacent(src_cell)
+
+        for i in adjacent.length
+            if(cells[adjacent[i][0]][adjacent[i][1]].isOccupied)
+            else
+                return true
+            end
+        end
+
+        false
+    end
+
+    def makeChoice(src_cell)
     
     end
 
-    def canMove(int[2] src_cell)
+    #piece in this method is an instance of the piece class (clearly but you never know)
+    def placePiece(dest_cell, piece)
     
     end
 
-    def makeChoice(int[2] src_cell)
+    def takePiece(dest_cell)
     
     end
-
-    def placePiece(int[2] dest_cell, Piece piece)
-    
-    end
-
-    def takePiece(int[2] dest_cell)
-    
-    end
-
-    def validateOwner(int[2] dest_cell, Player owner)
+    #in this method owner is an instance of the player class
+    def validateOwner(dest_cell,owner)
     
     end
 
     def printBoard
     
     end
-
-    def countPlayerPieces(Player owner)
+    #in this method owner is an instance of the Player class
+    def countPlayerPieces(owner)
     
+    end
+
+    private
+    def getAdjacent(center)
+        adjacent = [[center[0]-1,center[1]],[center[0]+1,center[1]],[center[0],center[1]-1],center[0],center[1]+1]
+        adjacent
     end
 end
