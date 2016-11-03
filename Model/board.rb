2d99cfc2344313d11de1544b01f592bb621bfa8a
bgraansm <bgraansm@mail.uoguelph.ca>
2016-11-03 17:04:08 -0400

    duck type!

--- Model/board.rb
+++ Model/board.rb
@@ -4,7 +4,7 @@ require_relative 'piece'
 
 class Board
   
-    @cells = cell[5][6]
+    @cells = Array.new
         
     #for all the following src_cell and dest_cell should be a integer array of size 2
     def jump(src_cell, dest_cell)
